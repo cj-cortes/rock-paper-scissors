81171b547633ada1dafff0c843cbfb9c7c1d5bbd
CjCortes <cjcortescodes@gmail.com>
2024-01-26 05:47:16 +0800

    Update playRound function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let choices = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let compScore = 0;
 
-// Create function that randomly returns either "Rock," "Paper," or "Scissors"
+// Create function that randomly returns either "rock," "raper," or "scissors"
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * choices.length);
   let computerChoice = choices[randomChoice];
@@ -18,16 +18,16 @@ function playRound(playerSelection, computerSelection) {
       (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
     playerScore++;
-    return 'You won!';
+    return `You won! ${playerSelection} beats ${computerSelection}.`;
   } else if (
       (playerSelection === 'rock' && computerSelection === 'paper') ||
       (playerSelection === 'paper' && computerSelection === 'scissors') ||
       (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
     compScore++;
-    return 'You lost!';
+    return `You lost! ${computerSelection} beats ${playerSelection}.`;
   } else {
-    return 'You tied!';
+    return `You tied! Both picked ${playerSelection}`;
   }
 }
 
@@ -41,10 +41,10 @@ function game() {
   while (compScore < 3 && playerScore < 3) {
     let playerSelection = prompt('Pick rock, paper, or scissors:').toLowerCase(); // prompts the player to choose and make the input case-insensitive
     const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
 
     console.log('Computer chose:', computerSelection);
     console.log('You chose:', playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
     console.log(`Your score: ${playerScore},`,`Computer score: ${compScore}`);
   }
 
